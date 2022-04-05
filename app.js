60bd42c71028cdb2feb74ace6ecdd891222712aa
Lloyd <elloyd@FVFZMYHCJ1WK.tld>
2022-04-05 14:37:50 -0500

    I did a stupib

---
+++ app.js
@@ -0,0 +1,6 @@
+function run() {
+    document.getElementById("paragraph").innerHTML = "Hello World";
+    document.getElementById("paragraph").style.backgroundColor = "blue";
+    document.getElementById("paragraph").style.color = "#000000";
+}
+
